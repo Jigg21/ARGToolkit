1eab11623dd98e2a661b7a213b46fd5910a9afc5
Torry Mathis <to.mathis21@gmail.com>
2020-05-30 09:37:43 +0200

    Added Basic Bruteforce for Vignere Ciphers

--- CodeSolver/resultChecker/resultChecker.py
+++ CodeSolver/resultChecker/resultChecker.py
@@ -19,9 +19,15 @@ def acceptResult(resultString,langDictionary,tolerance):
 def dictionaryBruteForce(wordDictionary,cipherText):
     possibleDict = dict()
     wordsChecked = 0
+    total = len(wordDictionary)
+    milestone = 1
+    print("Starting Dictionary Bruteforce")
+    print("Percent Done: 0",end="\r")
     for key in wordDictionary:
         wordsChecked += 1
-        print("Words Checked: ", wordsChecked,end="\r")
+        if (wordsChecked/total > .05*milestone):
+            print("Percent Done:", 5*milestone,end="\r")
+            milestone += 1
         answer = vigdecipher(key,cipherText)
         plausibility = checkResult(answer,wordDictionary)
         if (plausibility > .8):
@@ -31,7 +37,54 @@ def dictionaryBruteForce(wordDictionary,cipherText):
     print("Most Probable Cipher Key:",best.split(":")[0])
     print("Most Probable Text:",best.split(":")[1])
 
+def absoluteBruteForce(wordDictionary,cipherText,placeMax):
+    print("Generating Permutations")
+    wordlist = list()
+    generateLetterPerms(placeMax,"",wordlist)
+    possibleDict = dict()
+    wordsChecked = 0
+    total = len(wordlist)
+    milestone = 1
+    print("Complete!")
+    print("Starting Simple Bruteforce")
+    print("Percent Done: 0",end="\r")
+    for item in wordlist:
+        wordsChecked += 1
+        if (wordsChecked/total > .05*milestone):
+            print("Percent Done:",5*milestone,end="\r")
+            milestone += 1
+        answer = vigdecipher(item,cipherText)
+        plausibility = checkResult(answer,wordDictionary)
+        if (plausibility > .8):
+            id = item + ":" + answer
+            possibleDict[id] = plausibility
+        if (plausibility == 1):
+            print("Found 100% match")
+            print("Key:" + item)
+            print("Text:" + answer)
+            print()
+            print("Continue? ")
+            userInput = input("(Y/N)")
+            if (userInput == "y" or userInput == "Y"):
+                pass
+            else:
+                break
+    best = max(possibleDict,key=possibleDict.get)
+    print("Most Probable Cipher Key:",best.split(":")[0])
+    print("Most Probable Text:",best.split(":")[1])
+
+def generateLetterPerms(depth,currentOutput,finalList):
+    if depth == 0:
+        finalList.append(currentOutput)
+        return
+    for letter in letters:
+        generateLetterPerms(depth-1,currentOutput+letter,finalList)
+    finalList.append(currentOutput)
+    return
+
 def vigdecipher(key,cipherText):
+    if (len(key) == 0):
+        return ""
     keynum = 0
     result= ""
     for letter in cipherText:
@@ -51,7 +104,11 @@ def vigdecipher(key,cipherText):
     return result
 
 
-cipher = "khz scgrzwfvd vvr rlgsjvd jrpv eqiep fza lvamw gf dzgvue rlvth keekixyyrr micieniakaomivs jj gye jtcienwveg xpnjs vvr ko micieniak aih evpmifj tciz"
+#cipher = "hes ioph zomwqoiwph ts eoku pvxzi pb hes lbb kec pcir rg qvb fldb"
+cipher = "ievwbe hvw ncwvcf vxdwgvd wyg eoo eynatw ve a minjoieoce ased"
 with open("Dictionary.json") as f:
+    print("Loading Dictionary")
     words = json.load(f)
-    dictionaryBruteForce(words,cipher)
+    print("Loaded!")
+    #dictionaryBruteForce(words,cipher)
+    absoluteBruteForce(words,cipher,5)
